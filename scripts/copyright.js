6787425df39350fd700b729d2e69dd80fbcac0c3
k8w <me@k8w.io>
2021-05-08 19:45:14 +0800

    MIT License & new build scripts

---
+++ scripts/copyright.js
@@ -0,0 +1,7 @@
+module.exports = `/*!
+ * TSRPC Miniapp v${require('../package.json').version}
+ * -----------------------------------------
+ * Copyright (c) Kingworks Corporation.
+ * MIT License
+ * https://github.com/k8w/tsrpc-browser
+ */`
